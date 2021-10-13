246bbc372779f0071c6633d3225ae5d21105b99a
quandangv <quandangv@gmail.com>
2021-10-13 19:55:07 +0700

    Add example config file

---
+++ server_conf.example.py
@@ -0,0 +1,5 @@
+TOKEN = '**bot token here**'
+GAME_CHANNEL = **game channel id here**
+DEBUG_CHANNEL = **debug channel id here**
+ADMINS = [ **admin1 id**, **admin2 id** ]
+LANGUAGE = '**path to a language file in /lang without the file extension**'
